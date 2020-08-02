bbbec7eb67db87758b14bef0ffe261e310e86677
antonio <gantoniodeveloper@outlook.com>
2020-08-02 12:24:20 -0500

    negocio

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -71,8 +71,67 @@ class PostController {
               })
 
           }
-        }
-    }
+        }else if(data.type == 'negocio'){
+            const rules = {
+                text: 'required|string|max:1500|min:300',
+                name: 'required|string|min:20|max:150',
+                images: 'required',
+                location : 'required|max:90|string',
+                type: 'required|string|min:7|max:10',
+                category: 'required|max:150'
+            }
+    
+            const messages = {
+                required: 'Es necesario llenar todos los campos',
+                'images.required' : 'Necesitas subir al menos una imagen',
+                'text.min': 'El post debe tener al menos 300 catacteres',
+                'text.max':'El post no debe exceder los 1500 caracteres',
+                'name.min': 'Nombre debe tener al menos 20 caracteres',
+                'name.max': 'Nombre no puede tener más de 150 caracters',
+              }
+    
+              const validation = await validate(data, rules, messages)
+              if(validation.fails()){
+    
+                const message = validation.messages()
+                let error = message[0]
+                return response.status(400).json({
+                    status: 'wrong',
+                    message: error.message
+                })
+    
+              } else {
+                  const post = await new Post()
+                  post.user_id = auth.current.user.id
+                  post.name = data.name
+                  post.type = data.type
+                  post.location = data.location
+                  post.category = data.category
+                  post.text = data.text
+                  await post.save()
+                  
+                  const posto = post.toJSON()
+    
+                  for (let image of data.images) {
+                    const pick = image['base64'];
+                    const resultado = await Cloudinary.v2.uploader.upload(pick);
+        
+                    const postimage = await new Postimage()
+                    postimage.post_id = posto.id
+                    postimage.url = resultado.secure_url
+                    postimage.publicid = resultado.public_id
+                    await postimage.save()
+                  }
+    
+                  return response.json({
+                      status : 'sure',
+                      data: post
+                  })
+    
+            }
+            
+        } 
+    } 
 }
 
 module.exports = PostController
