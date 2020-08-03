075c25ab6192f83bfe1e4b0e952a417f687cda69
antonio <gantoniodeveloper@outlook.com>
2020-08-03 04:46:29 -0500

    ==

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -6,20 +6,7 @@ const { validate } = use('Validator')
 const Cloudinary = use('Cloudinary')
 const fs = use('fs');
 const {Storage} = use('@google-cloud/storage');
-
-const keys = {
-    type: "service_account",
-    project_id: "intense-howl-247800",
-    private_key_id: "e345fd9380e87d7d1edf0095c69d45180727e354",
-    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDH1B8odRceGFWt\nfBxlnoA9+mUFEhcZXCx0yV3KRYT1SxVWrG8ClmkSJkZ4dogtkBE/1nsNiEzT7P28\ngWuEzvw5YlLnAZu7+BEUmc2IMa7mPlo7aNwZ986XC8vb+lBkOPi9UTXzZmXon+Z7\nhZR1FKNq0w7Rqft6p7aZ7iAuFOsZrqkRABKXUzGq8gPvwCjUHeZp+uMb/RQAg3/N\niZMntpGb4px/0QymAhpaBGeBC+l3egjfCXNP2TD8EXZg97V86bG9uDmw1RdVpgvr\nNppTW8Bn99oZnN1FJzys4DpT1o0PiW2jhbj26UChXk2KGYo9O5Egjn2Rltd4Wl9J\ns592Hhc/AgMBAAECggEACld3LVf7HXGcZGE7GCPRP9GbDxRoT1f32r25I3Zyu/TX\nq96oHKy9z1SmoZOnskP1nAEHDdtg0xOGysSyMBfyzSswFXY0tD32FFYAPMh3v+WT\n6woMzp6rHiITAx6XQxQA4BbLk7TqYdUQGNvbf1KMHNXDckk5gSocRycgCF3tWXX2\nkzmecvavFbLfcA/XR7imQnuLuF9cLuw77cGxIa8ClYiM1mtZrGPJpVZ0tCccS9XY\nCQH6bKNOtG26F4+nAcAtEyl+7GNtv7zKuudIW0X1sc/pQqdVn3xJLZDrY+WFU8BD\n4SJeqN8fjQffpWkbJ/Gm5ePZnxRi1Zl/JtrZhGJfAQKBgQDvUdqHomJobh9LbJ+q\nUebjRdELO40Q/wl/iAZS4jTwhFbrR6xX2RVBdcgtcCSeRA3fbbLcv94p9fBBK1sX\ndxABOKcTQEamoTQwx1DRCJq+/oXAWxshX/LMg5g+Y/+mHA/+42sO2DvsmiVZbmYb\n7SJj5HuGCCrv4yqEaaIFvt+lfwKBgQDVwaIxRDb/o6CgGDfbE+oXPaWGlQzrx8Id\niUGEoxalV6jXiDjiXZ/8fzdkogIFsI322e2hV3T8IcPmNI2FnO7N4HXPUz6Ncw++\nDN10C82MTwldKEg4alVcdj1o4xyoymFfSlbu4jmI2eoU8Pd4J1KFBJLXkl7Xtj4u\npqP/8wfuQQKBgC3eYWUXsYbUfHkHU77gPcGXsw5VFp+IUji4SIYARydGJiaoMRcR\nS2qleX2D/fC7mHEXZG289IbUVX0YIzhnIJErW5JsAl6TjR5ARXQbExXCUL5id9u4\nuR+LpVVn8yrSPBqdWfqZZTre/IOcjkOR+sJsm2RAEoKx5odEe8JgKBtfAoGBAKnk\n6j3QIwqSiiNUvGhi7qckN0C9jPSF49JH+owqd+BYyPjt65B3q6YrkuJoJcVR67Ps\nyIN5GKEhUSvSTz2IWwHFSr3LQyd38WYzoXwPuYKwexfQ4X39ffUsgWixQmEh4by0\nQF5D7kjIMtYMJBUa4+qXlQLD8p/u2IOJrUXMa1xBAoGALyUYP0lz8l1/iThPGj0V\nvmbP2L5WC1A5YbCCn5DoYUVjvVEv/D/AMBqb+7TkFpfJLBJ1SbqrjaZ20HStLfGD\nJINa6ukUJ14GY6X2qfkw5JLYxxddsXmXC4JTJ70mtzxOl1DW6KednY39bOTnLPql\nN7TekvjGgcv3RPZpHDTMA9U=\n-----END PRIVATE KEY-----\n",
-    client_email: "buscodeveloper@intense-howl-247800.iam.gserviceaccount.com",
-    client_id: "102917404767801708120",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/buscodeveloper%40intense-howl-247800.iam.gserviceaccount.com"
-  }
-  
+const path = use('path')
 
 
 class PostController { 
@@ -212,12 +199,11 @@ class PostController {
     }
     
     async curriculum({auth, request, response}){
-      const SCOPES = ['https://www.googleapis.com/auth/drive'];
-      fs.readFile('~/intense-howl-247800-e345fd9380e8.json', (err, content) => {
-        if (err) return console.log('Error loading client secret file:', err);
-        authorize(JSON.parse(content), listFiles);
-      });
-
+      const gc = new Storage({
+        keyfilename : path.join(__dirname, '../../petras-a108b-776bf147ea41.json'),
+        project_id : 'petras-a108b'
+      })
+      gc.getBuckets().then(x => console.log(x))
     }
 }
 
