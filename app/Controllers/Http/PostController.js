da4900aae66f0db50f0a8444e919d86a45432f29
antonio <gantoniodeveloper@outlook.com>
2020-08-03 04:55:38 -0500

    hhh

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -200,7 +200,7 @@ class PostController {
     
     async curriculum({auth, request, response}){
       const gc = new Storage({
-        keyfilename : path.join(__dirname, '../../petras-a108b-776bf147ea41.json'),
+        keyfilename : path.join(__dirname, '~/petras-a108b-776bf147ea41.json'),
         project_id : 'petras-a108b'
       })
       gc.getBuckets().then(x => console.log(x))
