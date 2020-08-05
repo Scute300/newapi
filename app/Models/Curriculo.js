1d67307504956bd412262e5b1b5196d658731f62
antonio <gantoniodeveloper@outlook.com>
2020-08-04 21:12:01 -0500

    =

--- app/Models/Curriculo.js
+++ app/Models/Curriculo.js
@@ -3,10 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Curiculo extends Model {
+class Curriculo extends Model {
     user(){
         return this.belongsTo('App/Models/User')
     }
 }
 
-module.exports = Curiculo
+module.exports = Curriculo
