b4cbc9c3d25c4c61589248f3cce79a51d0e75845
antonio <gantoniodeveloper@outlook.com>
2020-08-04 20:30:45 -0500

    =

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -200,7 +200,7 @@ class PostController {
       const link = request.only(['image'])
       const user = auth.current.user
       try{
-        let cv = await Document.findByOrFail('user_id', user.id)
+        let cv = await Curriculo.findByOrFail('user_id', user.id)
 
         console.log(cv)
 }catch(error){
