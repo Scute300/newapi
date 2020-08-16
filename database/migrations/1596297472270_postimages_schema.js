a08ef3a3fb3c2ec52b27fa7425eafacdc2441599
antonio <gantoniodeveloper@outlook.com>
2020-08-16 12:54:07 -0500

    =

---
+++ database/migrations/1596297472270_postimages_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostimagesSchema extends Schema {
+  up () {
+    this.create('postimages', (table) => {
+      table.increments()
+      table.integer('post_id').references('id').inTable('posts').onDelete('CASCADE')
+      table.text('url', 300).notNullable()
+      table.string('publicid',100).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('postimages')
+  }
+}
+
+module.exports = PostimagesSchema
